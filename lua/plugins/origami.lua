99eb1e44fd6c6d8f6f090ff11940d958855576c5
Markos Narinian <manarinian@gmail.com>
2026-05-13 08:55:02 +0300

    msg

---
+++ lua/plugins/origami.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+	"chrisgrieser/nvim-origami",
+	event = "VeryLazy",
+	opts = {}, -- required even when using default config
+
+	-- recommended: disable vim's auto-folding
+	init = function()
+		vim.opt.foldlevel = 99
+		vim.opt.foldlevelstart = 99
+	end,
+}
--- lua/plugins/visual-multi.lua
+++
@@ -1,4 +0,0 @@
----@type LazySpec
-return {
-  'mg979/vim-visual-multi'
-}
