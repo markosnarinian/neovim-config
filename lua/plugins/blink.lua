771d60304fbcda167b74dcc5dd8aab094e2dcf64
Markos Narinian <manarinian@gmail.com>
2026-06-25 22:25:36 +0300

    .

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,9 +7,9 @@ return {
     "rafamadriz/friendly-snippets",
   },
   build = function()
-    -- build the fuzzy matcher, wait up to 60 seconds
+    -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
     -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
-    require("blink.cmp").build():wait(60000)
+    require("blink.cmp").build():pwait()
   end,
 
   ---@module 'blink.cmp'
